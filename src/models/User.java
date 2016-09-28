ba429be13ce9e3027376bddf6b79f5b4de480068
ciara <ciarapower88@gmail.com>
2016-09-28 10:38:06 +0100

    Introduced HashMap

--- src/models/User.java
+++ src/models/User.java
@@ -1,5 +1,6 @@
 package models;
 import static com.google.common.base.MoreObjects.toStringHelper;
+import com.google.common.base.Objects;
 
 public class User 
 {
@@ -19,6 +20,7 @@ public class User
     this.email = email;
     this.password = password;
   }
+  @Override
   public String toString()
   {
     return toStringHelper(this).addValue(firstName)
@@ -27,4 +29,10 @@ public class User
                                .addValue(email)                               
                                .toString();
   }
+  
+  @Override  
+  public int hashCode()  
+  {  
+     return Objects.hashCode(this.lastName, this.firstName, this.email, this.password);  
+  }  
 }
